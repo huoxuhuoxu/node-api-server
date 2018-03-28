b4343decd9e189924c38d5cf1c60283d2d508aae
huzhen <zhen.hu@evenhidata.com>
2018-03-28 11:17:15 +0800

    完善异步错误捕获

--- index.js
+++ index.js
@@ -8,7 +8,38 @@ if (!module.parent) {
     const server = http.createServer(app);
     server.listen(PORT);
 
-    console.log(`工作进程 ${process.pid} 已启动, 监听端口号 ${PORT}...`);
+    server.on("listening", () => {
+        console.log(`工作进程 ${process.pid} 已启动, 监听端口号 ${PORT}...`);
+    });
+
+    server.on("error", (err) => {
+        if (err.syscall !== "listen") throw err;
+
+        switch (error.code) {
+            case 'EACCES':
+                console.error(PORT + ' requires elevated privileges');
+                process.exit(1);
+                break;
+            case 'EADDRINUSE':
+                console.error(PORT + ' is already in use');
+                process.exit(1);
+                break;
+            default:
+                throw error;
+        }
+        
+    });
+
+    process.on("uncaughtException", (msg) => {
+        console.log("[warning] uncaughtException: %s", msg);
+    });
+
+    process.on("unhandledRejection", (rejected) => {
+        console.log("[warning] unhandledRejection: %s", rejected);
+    });
+
+    // ...
+
 }
 
 
